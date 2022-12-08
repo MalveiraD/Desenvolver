38c3e792ae5a7feefdb8b513d4abfc43f3984d55
MalveiraD <45291674+MalveiraD@users.noreply.github.com>
2022-12-08 10:42:48 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -222,7 +222,7 @@ jQuery(function ($) {
     /* Vimeo API: http://developer.vimeo.com/player/js-api */
     
         var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
+        $f == Froogaloop
         var player = $f(iframe);
 
         $('.modal').on('hidden.bs.modal', function () {
